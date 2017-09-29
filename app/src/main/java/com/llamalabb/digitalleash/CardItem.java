32bed415c8097c440d8a861bfdb9a1e43c8a29d5
Andrew O'Brien <andygobrien@gmail.com>
2017-09-29 15:35:28 -0400

    added files that were missing

---
+++ app/src/main/java/com/llamalabb/digitalleash/CardItem.java
@@ -0,0 +1,21 @@
+package com.llamalabb.digitalleash;
+
+/**
+ * Created by andy on 9/28/17.
+ */
+
+public class CardItem {
+
+    private String mIntroMsg;
+
+
+
+    public CardItem(String msg) {
+        mIntroMsg = msg;
+    }
+
+    public String getText() {
+        return mIntroMsg;
+    }
+
+}
